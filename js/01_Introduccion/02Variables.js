5f2cdcc8856d386f5cef342f770626c00664c561
Ubaldo Narvaez <satou.matsuzaka.3443@gmail.com>
2025-12-23 15:19:36 -0600

    Se agrega el apunte de Variables

---
+++ js/01_Introduccion/02Variables.js
@@ -0,0 +1,41 @@
+/* REGLAS:
+- Las variables deben de tener nombres unicos
+- En los nombres de variables solo se puede agregar guion bajo como simbolo
+- No nombrar las variables como funciones de javascript
+*/
+
+/* Tipos de datos que se pueden guardar
+
+    string - Cadena de Texto
+    number - Numero
+    boolean - Booleano (Verdadero o Falso)
+    object - Objeto
+    function - Funciones
+
+    null - nulo
+    undefined - Indefinido
+
+*/
+
+// Asi se declaran las variables:
+let nombre = 'Neasaka '; // Solo se puede acceder dentro de un bloque
+let apellido = 'Pendragon'
+const correo = 'correo@correo.com'; //  Solo se puede usar dentro de un bloque y no se pueden redefinir los valores de la variable
+var numero1 = 5;
+var numero2 = 2;
+
+// Declarar varias variables:
+let telefono, pais, id;
+telefono = 123456789
+
+const resultado = 4 + 4;
+const nombreCompleto = nombre + apellido
+
+
+// Impresiones de los resultados
+alert(nombre); // Alerta
+console.log(numero1 + numero2); // Suma de dos variables en consola
+//document.write(numero1); // Se muestra dentro de la pagina
+console.log(telefono)
+console.log(resultado)
+console.log(nombreCompleto)
